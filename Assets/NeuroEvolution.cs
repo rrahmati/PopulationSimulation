240da808f6b2818cd27fd8ccfdc966b588da58d5
rrahmati <rrahmati@gmail.com>
2015-10-11 15:04:51 -0400

    rtNeat added.

---
+++ Assets/NeuroEvolution.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class NeuroEvolution : MonoBehaviour {
+
+    private string sceneStateFileName = "rtNEAT.1.0.2\\sceneState";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        writeSceneState();
+	}
+
+    void writeSceneState()
+    {
+        string lines = "";
+        File.WriteAllText(sceneStateFileName, lines);
+    }
+}
