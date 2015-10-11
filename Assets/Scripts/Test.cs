e95e8be4989505760388d51816b6172a925c066f
rrahmati <rrahmati@gmail.com>
2015-10-11 14:43:40 -0400

    First commit.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Test : MonoBehaviour {
+
+	[DllImport("testUnity")]
+	private static extern float foofoo(float a, float b);
+
+	void Awake(){
+		Debug.Log(foofoo(5,foofoo(5,20)));
+	}
+}
