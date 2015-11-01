fe2c8ed167324698d1d4f81af0391292864c1566
Hung-Le-UCF <hungle.zero@yahoo.com>
2015-11-01 16:05:54 -0500

    Update WorldController.cs
    - WorldController now perform evaluation every inverval.
    - WorldController now can read file and do thing based on the content in
    that file. (WIP)

--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class WorldController : MonoBehaviour {
 
@@ -9,23 +10,31 @@ public class WorldController : MonoBehaviour {
 	public GameObject spawnPoint;
 	public GameObject spawnObject;
 
-	private string NNInputFileName = "rtNEAT.1.0.2\\NNinput";
-	private string NNOutputFileName = "rtNEAT.1.0.2\\NNoutput";
+    private string NNInputFileName = "rtNEAT.1.0.2\\in_out\\Fitness_input";
+	private string NNOutputFileName = "rtNEAT.1.0.2\\Fitness_output";
 	
 	public ArrayList agentList;
 
+    public float EvaluationRateInSec = 30;
+    private float timer = 0;
 
+    private float[] fitnessList;
 
 	// Use this for initialization
 	void Start () {
 		agentList = new ArrayList ();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		SpawnAgent ();
 
+        SpawnAgent ();
+
+        if (timer > EvaluationRateInSec) {
+            EvaluatePopulation();
+        }
+
+        timer += Time.deltaTime;
 
 	}
 
@@ -51,11 +60,55 @@ public class WorldController : MonoBehaviour {
 	}
 
 
-	void Evalutation(){
+	void EvaluatePopulation(){
 		// get data from each agent
+        float[] fitnessList = new float[agentList.Count];
 
+        for (int i = 0; i < agentList.Count; i++) {
+            fitnessList[i] = FitnessFunction(((GameObject)agentList[i]).GetComponent<Agent>());
+        }
 
 
 	}
 
+    float FitnessFunction(Agent agentScript) {
+
+
+
+
+        return 0;
+    }
+
+    void WriteFitnessInput() {
+        string path = NNInputFileName;
+        //print(path);
+        StreamWriter file = new StreamWriter(path);
+
+        string lines = "";
+        foreach (float input in fitnessList) {
+            lines += input + ",";
+        }
+        lines += "\n";
+
+        file.WriteLine(lines);
+
+        file.Close();
+    }
+
+    void ReadFitnessOutput() {
+        string path = NNOutputFileName + "_" + ID;
+
+        if (!File.Exists(path))
+            return;
+
+        StreamReader reader = new StreamReader(File.OpenRead(path));
+
+        string line = reader.ReadLine();
+        string[] values = line.Split(',');
+        for (int output = 0; output < values.Length; output++) {
+            // do something based on this value
+        
+        }
+        //print(outputArray);
+    }
 }
